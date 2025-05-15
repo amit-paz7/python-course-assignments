3f4c00e5f3b6b805594cfc30df4f6d1ad41e1f68
amit-paz7 <amitpaz7@gmail.com>
2025-05-15 15:49:04 +0300

    Create number_guessing_game_3.py

---
+++ day04/number_guessing_game_3.py
@@ -0,0 +1,19 @@
+import random
+secret_number = random.randint(1, 20)
+while True:
+    guess_str = input("Guess a whole number between 1 and 20 (or 'x' to exit, 's' for secret): ")
+    if guess_str.lower() == 'x':
+        print("Exiting the game.")
+        break
+    elif guess_str.lower() == 's':
+        print(f"The secret number is: {secret_number}")
+        continue
+    try:
+        guess = int(guess_str)
+        if guess == secret_number:
+            print("Congratulations! You guessed the number!")
+            break
+        elif guess < secret_number:
+            print("Your guess is smaller than the secret number. Try again.")
+        else:
+            print("Your guess is bigger than the secret number. Try again.")
