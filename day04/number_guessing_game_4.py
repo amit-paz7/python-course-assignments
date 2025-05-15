5ecf0039ce3d036979f698d3f70cd1e1f7ca7345
amit-paz7 <amitpaz7@gmail.com>
2025-05-15 15:54:22 +0300

    Create number_guessing_game_4.py

---
+++ day04/number_guessing_game_4.py
@@ -0,0 +1,30 @@
+import random
+secret_number = random.randint(1, 20)
+debug_mode = False
+while True:
+    if debug_mode:
+        print(f"(Debug Mode) The secret number is: {secret_number}")
+    guess_str = input("Guess a whole number between 1 and 20 (or 'x' to exit, 's' for secret, 'd' for debug): ")
+    if guess_str.lower() == 'x':
+        print("Exiting the game.")
+        break
+    elif guess_str.lower() == 's':
+        print(f"The secret number is: {secret_number}")
+        continue
+    elif guess_str.lower() == 'd':
+        debug_mode = not debug_mode
+        if debug_mode:
+            print("Debug Mode is now on.")
+        else:
+            print("Debug Mode is now off.")
+        continue
+    try:
+        guess = int(guess_str)
+        if guess == secret_number:
+            print("Congratulations! You guessed the number!")
+            break
+        elif guess < secret_number:
+            print("Your guess is smaller than the secret number. Try again.")
+        else:
+            print("Your guess is bigger than the secret number. Try again.")
+   
